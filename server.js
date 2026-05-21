d7603feca89074b91e1e4be727966b568cac873f
gautamchintu10-sys <gautamchintu10@gmail.com>
2026-05-22 01:24:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors'); 
-const Property = require('./models/Property'); 
+const property = require('./models/Property'); 
 
 const app = express();
 
